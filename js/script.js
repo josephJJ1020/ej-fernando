3b2c4318bd84de975dea56da6c33ceb48fa1769c
josephJJ1020 <the.josephfernando@gmail.com>
2022-09-05 06:02:43 +0800

    Added responsive design and new images

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-console.log('hello world')
+console.log("hello world");
 
 // set html scroll behavior to smooth when clicking anchor tag
-document.addEventListener('click', function(e){  
-    var anchor = e.target.closest('a');
-    if(anchor !== null) {
-        console.log('hello world');
-        document.querySelector('html').style.scrollBehavior = 'smooth';
-      //do something with a tag
-    }
-  });
+document.addEventListener("click", function (e) {
+  var anchor = e.target.closest("a");
+  if (anchor !== null) {
+    console.log("hello world");
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    //do something with a tag
+  }
+});
+
+// project container animation
+const projectContainers = document.querySelectorAll('.project-container')
+
+projectContainers.forEach(container => {
+  container.addEventListener('click', function() {
+    container.classList.toggle('project-clicked')
+  })
+
+})
+gsap.from('nav', {duration: 1, y: '-100%'})
