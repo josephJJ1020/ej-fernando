01a91dd84985e2cd72c6917391768b0f86949837
josephJJ1020 <the.josephfernando@gmail.com>
2022-09-06 20:11:04 -0500

    Adds navbar on mobile sizes and implements gsap for animations

--- js/script.js
+++ js/script.js
@@ -10,13 +10,80 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// project container animation
-const projectContainers = document.querySelectorAll('.project-container')
+// nav hamburger hide nav functionality
+// only when screen is less than 500px
 
-projectContainers.forEach(container => {
-  container.addEventListener('click', function() {
-    container.classList.toggle('project-clicked')
-  })
+var searchClickEvent = function () {
+  let navUl = document.querySelector("nav ul");
+  if (window.matchMedia("(max-width: 500px)").matches) {
+    console.log("hamburger");
+    navUl.classList.add("hidden");
+    document.querySelector(".hamburger").addEventListener("click", () => {
+      navUl.classList.toggle("hidden");
+    });
+  } else {
+    navUl.classList.remove("hidden");
+    console.log("err");
+  }
+};
 
-})
-gsap.from('nav', {duration: 1, y: '-100%'})
+
+window.addEventListener("resize", searchClickEvent);
+window.addEventListener('load', searchClickEvent)
+
+
+// project container click animation
+const projectContainers = document.querySelectorAll(".project-container");
+
+projectContainers.forEach((container) => {
+  container.addEventListener("click", function () {
+    container.classList.toggle("project-clicked");
+  });
+});
+
+// nav animation on start
+gsap.from("nav", { duration: 1, y: "-100%" });
+
+// about page animation
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#about",
+    start: "center bottom",
+  },
+});
+
+tl.from(".about-detail.left", { x: -500, opacity: 0, duration: 1 }).from(
+  ".about-detail.right",
+  { x: 500, opacity: 0 },
+  "-=0.7"
+);
+
+// skills page animation
+
+const skills = gsap.utils.toArray(".skill");
+skills.forEach((skill) => {
+  gsap.from(skill, {
+    scale: 0,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".skills-container",
+      start: "center bottom",
+    },
+  });
+});
+
+// project container animations
+
+const projects = gsap.utils.toArray(".project-container");
+projects.forEach((project) => {
+  gsap.from(project, {
+    y: 500,
+    duration: 1,
+    scrollTrigger: {
+      trigger: project,
+      start: "-150% bottom",
+    },
+  });
+});
